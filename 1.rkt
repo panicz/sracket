aa5aa0d000afb2005a56d9386555b0f8a8568bd4
Panicz Maciej Godek <godek.maciek@gmail.com>
2019-01-28 20:01:54 +0100

    Ported to grand syntax

--- 1.rkt
+++ 1.rkt
@@ -1,64 +1,15 @@
 #lang racket
 (require racket/match)
 (require "sracket.rkt")
+(require "grand-syntax.rkt")
 (require "ground-scheme.rkt")
 
 (slayer-init #:title "GRASP 1: draggable rectangles on the screen")
-
-(define (screen-size)
-  `(640 480))
 
 (keydn 'escape exit)
 
 (define ((_ . message) _)
   (apply _ message))
-
-(define stage
-  (match-let* ((elements '())
-	       (black 0)
-	       (`(,width ,height) (screen-size))
-	       (image (rectangle width height black))
-	       (hovered-element #false))
-    
-    (lambda message
-      (match message
-	(`(as-image)
-	 (fill-image! image black)
-	 (fold-right (lambda (element image)
-		       (match-let ((`(,x ,y) (element 'position)))
-			 (draw-image! (element 'as-image) x y image)
-			 image))
-		     image
-		     elements)
-	 image)
-	
-	(`(resize! ,new-width ,new-height)
-	 (set! width new-width)
-	 (set! height new-height)
-	 (set! image (rectangle width height 0)))
-	
-	(`(add! ,element)
-	 (set! elements `(,element . ,elements)))
-
-	(`(mouse-down ,x ,y)
-	 (when hovered-element
-	   (hovered-element 'mouse-down x y)))
-
-	(`(mouse-up ,x ,y)
-	 (when hovered-element
-	   (hovered-element 'mouse-up x y)))
-
-	(`(mouse-move ,x ,y ,dx ,dy)
-	 (let ((hovered (find (_ 'embraces? x y) elements)))
-	   (unless (eq? hovered hovered-element)
-	     (when hovered-element
-	       (hovered-element 'mouse-out))
-	     (when hovered
-	       (hovered 'mouse-over))
-	     (set! hovered-element hovered))
-	   (when hovered
-	     (hovered 'mouse-move x y dx dy))))
-	))))
 
 (define (draggable-rectangle left top width height color)
   (let ((dragged? #false)
@@ -81,10 +32,54 @@
 	(`(as-image)
 	 image)
 	(`(mouse-over)
-	 'highlight?)
+	 #false)
 	(`(mouse-out)
 	 (set! dragged? #false))
 	))))
+
+(define stage
+  (let* ((`(,width ,height) (screen-size))
+         (elements (map (lambda (_)
+	                  (draggable-rectangle
+			   (random (- width 50))
+			   (random (- height 50))
+			   50 50 (random #x1000000)))
+                        (range 10)))
+	 (image (rectangle width height 0))
+	 (hovered-element #false))
+    
+    (lambda message
+      (match message
+	(`(as-image)
+	 (fill-image! image 0)
+	 (fold-right (lambda (element image)
+		       (let ((`(,x ,y) (element 'position)))
+			 (draw-image! (element 'as-image) x y image)
+			 image))
+		     image
+		     elements)
+	 image)
+	
+	(`(mouse-down ,x ,y)
+	 (when hovered-element
+	   (hovered-element 'mouse-down x y)))
+
+	(`(mouse-up ,x ,y)
+	 (when hovered-element
+	   (hovered-element 'mouse-up x y)))
+
+	(`(mouse-move ,x ,y ,dx ,dy)
+	 (let ((hovered (find (lambda (_) (_ 'embraces? x y)) elements)))
+	   (unless (eq? hovered hovered-element)
+	     (when hovered-element
+	       (hovered-element 'mouse-out))
+	     (when hovered
+	       (hovered 'mouse-over))
+	     (set! hovered-element hovered))
+	   (when hovered
+	     (hovered 'mouse-move x y dx dy))))
+	))))
+
 
 (set-display-procedure! (lambda () (draw-image! (stage 'as-image))))
 
@@ -94,9 +89,3 @@
 		     (stage 'mouse-down x y)))
 (keyup 'mouse-left (lambda (x y)
 		     (stage 'mouse-up x y)))
-
-(match-let ((`(,width ,height) (screen-size)))
-  (for x in (range 10)
-    (stage 'add! (draggable-rectangle (random (- width 50))
-				      (random (- height 50))
-				      50 50 (random #x1000000)))))
